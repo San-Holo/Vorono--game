a39be9a6276ff690677ac127a553eda9183837ae
San-Holo <samy.kerben@gmail.com>
2018-11-27 15:56:36 +0100

    Update sat_solver.ml

--- sat_solver.ml
+++ sat_solver.ml
@@ -1,8 +1,3 @@
-(* Code extrait de:
-   SAT-MICRO: petit mais costaud !
-   par Sylvain Conchon, Johannes Kanig, Stéphane Lescuyer
-*)
-
 module type VARIABLES = sig
   type t
   val compare : t -> t -> int
